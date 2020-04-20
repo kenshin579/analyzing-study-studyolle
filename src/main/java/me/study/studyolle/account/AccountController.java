a281f5e11bd198f84befd435c1bcb66a65c75159
dongchul <sah3122@naver.com>
2020-04-20 13:05:56 +0900

    feat(Account): add sign up view

--- src/main/java/me/study/studyolle/account/AccountController.java
+++ src/main/java/me/study/studyolle/account/AccountController.java
@@ -9,7 +9,8 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
+        //model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm()); // class 이름과 동일한 attribute를 사용할 경우 생략 가능
         return "account/sign-up";
     }
-
 }
