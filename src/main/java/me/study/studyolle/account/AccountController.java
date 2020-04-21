5c6f6749ffc72682368895dc8af6cc655f4664ad
dongchul <sah3122@naver.com>
2020-04-21 13:12:32 +0900

    feat(SignUp): add SignUp & Init Binder Validate

--- src/main/java/me/study/studyolle/account/AccountController.java
+++ src/main/java/me/study/studyolle/account/AccountController.java
@@ -1,16 +1,42 @@
 package me.study.studyolle.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm") // signUpForm 데이터를 받을 때 데이터를 자동으로 바인딩 해준다. 여기선 validator 가 자동으로 실행된다.
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
         //model.addAttribute("signUpForm", new SignUpForm());
         model.addAttribute(new SignUpForm()); // class 이름과 동일한 attribute를 사용할 경우 생략 가능
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        //회원 가입 처리
+        return "redirect:/";
     }
 }
